1c325856f51eac78875ae1c8c7712e975ef7dff0
kalpanabaheti <45555769+kalpanabaheti@users.noreply.github.com>
2022-02-25 05:32:37 +0530

    Create reference_data.py
    This is an example of the kind of navigation possible.

---
+++ Code Intelligence/reference_data.py
@@ -0,0 +1,163 @@
+ython 3.6.1 (v3.6.1:69c0db5050, Mar 21 2017, 01:21:04) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: /Users/Kalpana/Documents/Navigation.py ==============
+
+Source filepaths: 
+1. /Kalpana Related /SENSE RESEARCH STUDY/100101
+2. /Kalpana Related /SENSE RESEARCH STUDY/11111111
+3. /Kalpana Related /SENSE RESEARCH STUDY/1110
+
+Enter source folder path: /Kalpana Related /SENSE RESEARCH STUDY/100101
+
+Iteration results of size 6
+['1', '10', '100', '1001', '10011', '100110']
+['1', '01', '001', '1001', '01001', '101001']
+['0', '10', '110', '0110', '00110', '100110']
+[0, 1, 2, 3, 4, 5]
+[5, 4, 3, 2, 1, 0]
+[4, 3, 2, 1, 0, -1]
+[1, 2, 3, 4, 5, 6]
+['10010', '1001', '100', '10', '1', '0']
+[10010, 1001, 100, 10, 1, 0]
+['0', '01', '011', '0110', '01100', '011001']
+[19, 9, 4, 2, 1, 0]
+['1', '0', '1', '0', '1', '0']
+/nInput controls 1 -
+                    
+1. Choose data point(s)
+                    
+2. Choose loops(s)
+                    
+3. Choose loop length(s)
+                    
+Enter as a comma separated series like this - 1,0,1
+                    
+Set 1 if you want it, 0 if you don't:
+
+1,0,1
+['1', '0', '1']
+
+Enter data point(s) comma separated: 
+1,2,3,4,5
+
+Enter data types of subsequence - int, str, bool: 
+int,int,int,int,int
+
+Enter lengths comma separated: 
+6
+Selected collection number:  259
+Random check: 
+2162265.py
+2984450.py
+9909944.py
+2434670.py
+1517785.py
+8147196.py
+
+If you want to run combinations again print y, else print n:y
+/nInput controls 1 -
+                    
+1. Choose data point(s)
+                    
+2. Choose loops(s)
+                    
+3. Choose loop length(s)
+                    
+Enter as a comma separated series like this - 1,0,1
+                    
+Set 1 if you want it, 0 if you don't:
+
+0,0,1
+['0', '0', '1']
+
+Enter lengths comma separated: 
+6
+Selected collection number:  330
+Random check: 
+2162265.py
+7613895.py
+6948540.py
+1686431.py
+6153516.py
+2756013.py
+
+If you want to run combinations again print y, else print n:y
+/nInput controls 1 -
+                    
+1. Choose data point(s)
+                    
+2. Choose loops(s)
+                    
+3. Choose loop length(s)
+                    
+Enter as a comma separated series like this - 1,0,1
+                    
+Set 1 if you want it, 0 if you don't:
+
+1,0,1
+['1', '0', '1']
+
+Enter data point(s) comma separated: 
+0,10,110
+
+Enter data types of subsequence - int, str, bool: 
+str,str,str
+
+Enter lengths comma separated: 
+6
+Selected collection number:  233
+Random check: 
+4985296.py
+1011514.py
+9909944.py
+1973247.py
+3358255.py
+2310772.py
+
+If you want to run combinations again print y, else print n:y
+/nInput controls 1 -
+                    
+1. Choose data point(s)
+                    
+2. Choose loops(s)
+                    
+3. Choose loop length(s)
+                    
+Enter as a comma separated series like this - 1,0,1
+                    
+Set 1 if you want it, 0 if you don't:
+
+1,0,1
+['1', '0', '1']
+
+Enter data point(s) comma separated: 
+-1,-2,-3,-4,-5
+
+Enter data types of subsequence - int, str, bool: 
+int,int,int,int,int
+
+Enter lengths comma separated: 
+6
+Selected collection number:  13
+3915735.py
+4644770.py
+7724672.py
+2617535.py
+5947659.py
+1147251.py
+2878409.py
+5206030.py
+7270048.py
+4192320.py
+3806480.py
+1505287.py
+4841409.py
+Random check: 
+3915735.py
+4841409.py
+
+If you want to run combinations again print y, else print n:n
+Do you want to choose another input file? y or n..:n
+>>> 
